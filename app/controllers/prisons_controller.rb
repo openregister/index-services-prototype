f38902c2c435c03ae98ceeb666cf15ef69b07a08
Dan Mitchell <dan@digitalreflow.co.uk>
2017-02-10 13:51:55 +0000

    Add column sorting

--- app/controllers/prisons_controller.rb
+++ app/controllers/prisons_controller.rb
@@ -1,9 +1,25 @@
 class PrisonsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
     if params[:search]
-      @prisons = Prison.search(params[:search])
+      @prisons = Prison.search(params[:search]).order("#{sort_column} #{sort_direction}")
     else
-      @prisons = Prison.all
+      @prisons = Prison.order("#{sort_column} #{sort_direction}")
     end
+  end
+
+  private
+
+  def sortable_columns
+    ["name", "organisation", "code", "opened", "closed", "address"]
+  end
+
+  def sort_column
+    sortable_columns.include?(params[:column]) ? params[:column] : "name"
+  end
+
+  def sort_direction
+    ["asc", "desc"].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def sort_link(column, title = nil)
+    title ||= column.titleize
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    css_class = sort_direction == "asc" ? "sorting-up" : "sorting-down"
+    css_class = column == sort_column ? css_class : ""
+    link_to title, { column: column, direction: direction }, class: css_class
+  end
 end
