6c8d75003200536352349af514ed30400481e036
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2017-02-27 17:02:21 +0000

    use grequests, in a synchronous manner

--- new-prisons.py
+++ new-prisons.py
@@ -25,11 +25,12 @@
 from binascii import hexlify
 import dateutil.parser
 import datetime
+import gevent.pool
+import grequests
 import hashlib
 import json
 import leveldb
 import psycopg2
-import requests
 
 
 POSTGRES_DB = "REGISTER_INDEX_SERVICE_DEVELOPMENT"
@@ -39,6 +40,7 @@ class RsfProcessor(object):
     def __init__(self):
         self.item_store = leveldb.LevelDB('./item_db')
         self.pgconn = psycopg2.connect("dbname="+POSTGRES_DB)
+        self.pool = gevent.pool.Pool(10) # 10 concurrent requests
         try:
             self.entries_processed = int.from_bytes(self.item_store.Get(b'entries_processed'), byteorder='big')
         except KeyError:
@@ -47,15 +49,46 @@ class RsfProcessor(object):
 
     def fetch_address(self, uprn):
         print('fetching address ' + uprn)
-        address_r = requests.get('https://address.discovery.openregister.org/record/%s.json' % uprn)
-        if address_r.status_code == 404:
+        address_req = grequests.get('https://address.discovery.openregister.org/record/%s.json' % uprn)
+        address_resp = grequests.send(address_req, self.pool).get().response
+        if address_resp.status_code == 404:
             print("WARNING: uprn %s resulted in 404" % uprn)
             return
-        address = requests.get('https://address.discovery.openregister.org/record/%s.json' % uprn).json()
+        address = address_resp.json()
         print('fetching street ' + address['street'])
-        street = requests.get('https://street.discovery.openregister.org/record/%s.json' % address['street']).json()
+        street_req = grequests.get('https://street.discovery.openregister.org/record/%s.json' % address['street'])
+        street = grequests.send(street_req, self.pool).get().response.json()
         address['street'] = street
         return address
+
+
+    def resolve_record(self, item_hash):
+        item_data = self.item_store.Get(item_hash)
+        item = json.loads(item_data.decode('utf-8'))
+        address = None
+        if 'address' in item:
+            address = self.fetch_address(item['address'])
+
+        street_name = None
+        if address:
+            street_name = address['street']['name']
+
+        end_date = None
+        start_date = None
+        if 'start-date' in item:
+            start_date = dateutil.parser.parse(item['start-date']).date()
+        if 'end-date' in item:
+            end_date = dateutil.parser.parse(item['end-date']).date()
+        with self.pgconn.cursor() as cursor:
+            cursor.execute("INSERT INTO prisons (name, code, address, created_at, updated_at, closed, opened) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                           (item['name'], item['prison'], street_name, datetime.datetime.now(), datetime.datetime.now(), end_date, start_date))
+            self.pgconn.commit()
+        
+
+
+    def resolve_records(self):
+        for code, item_hash in self.item_store.RangeIter(key_from=b'prison:', key_to=b'prison:ZZZ'):
+            self.pool.apply(self.resolve_record, [item_hash])
 
 
     def assert_root_hash(self, root_hash):
@@ -71,32 +104,7 @@ class RsfProcessor(object):
     def append_entry(self, timestamp, item_hash, key):
         self.item_store.Put(b'prison:'+key, item_hash)
 
-        self.entries_processed += 1
-
-
-    def resolve_records(self):
-        for code, item_hash in self.item_store.RangeIter(key_from=b'prison:', key_to=b'prison:ZZZ'):
-            item_data = self.item_store.Get(item_hash)
-            item = json.loads(item_data.decode('utf-8'))
-            address = None
-            if 'address' in item:
-                address = self.fetch_address(item['address'])
-
-            street_name = None
-            if address:
-                street_name = address['street']['name']
-
-            end_date = None
-            start_date = None
-            if 'start-date' in item:
-                start_date = dateutil.parser.parse(item['start-date']).date()
-            if 'end-date' in item:
-                end_date = dateutil.parser.parse(item['end-date']).date()
-            with self.pgconn.cursor() as cursor:
-                cursor.execute("INSERT INTO prisons (name, code, address, created_at, updated_at, closed, opened) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-                               (item['name'], item['prison'], street_name, datetime.datetime.now(), datetime.datetime.now(), end_date, start_date))
-                self.pgconn.commit()
-            
+        self.entries_processed += 1            
 
 
     def process(self, command, args):
@@ -118,14 +126,14 @@ class RsfProcessor(object):
 
 
 
-register_detail = requests.get('https://prison.discovery.openregister.org/register.json').json()
+register_detail = grequests.map([grequests.get('https://prison.discovery.openregister.org/register.json')])[0].json()
 total_entries = register_detail['total-entries']
 
 proc = RsfProcessor()
 entries = proc.entries_processed
 print('starting from entry %d' % entries)
 rsf_url = 'https://prison.discovery.openregister.org/download-rsf/%d/%d' % (entries, total_entries)
-rsf_req = requests.get(rsf_url, stream=True)
+rsf_req = grequests.map([grequests.get(rsf_url, stream=True)])[0]
 if rsf_req.status_code != 200:
     print("couldn't get RSF from url %s, got status %d" % (rsf_url, rsf_req.status_code))
     exit()
