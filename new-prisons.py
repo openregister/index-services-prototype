d2e1a594529620d933e7204d5ee4b8bd76b80e7c
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2017-03-01 17:03:48 +0000

    migrate from leveldb to redis
    we can run redis in heroku more easily than leveldb

--- new-prisons.py
+++ new-prisons.py
@@ -6,13 +6,13 @@
 # this script populates the local development postgres instance from the discovery registers
 # it works by doing the following:
 #   - download the prisons RSF
-#   - iterate through each item, store in a local levelDB instance
-#   - iterate through each entry, store as a record in a local levelDB instance (using primary key as key):
+#   - iterate through each item, store in redis
+#   - iterate through each entry, store as a record in redis (using primary key as key):
 #   - finally, for each record key:
 #     - if there's an address in the item, fetch it from the address register,
 #          fetch the corresponding street, and store its name
 #     - store in postgres
-#   - store the number of entries we've processed in levelDB so that we don't process them again
+#   - store the number of entries we've processed in redis so that we don't process them again
 #
 # This means that each time you run this script, it will only check
 # for new entries in the prison register, but it will re-check all
@@ -29,8 +29,8 @@ import gevent.pool
 import grequests
 import hashlib
 import json
-import leveldb
 import psycopg2
+import redis
 
 
 POSTGRES_DB = "REGISTER_INDEX_SERVICE_DEVELOPMENT"
@@ -38,13 +38,10 @@ POSTGRES_DB = "REGISTER_INDEX_SERVICE_DEVELOPMENT"
 
 class RsfProcessor(object):
     def __init__(self):
-        self.item_store = leveldb.LevelDB('./item_db')
+        self.item_store = redis.StrictRedis(host='localhost')
         self.pgconn = psycopg2.connect("dbname="+POSTGRES_DB)
         self.pool = gevent.pool.Pool(200) # concurrent requests
-        try:
-            self.entries_processed = int.from_bytes(self.item_store.Get(b'entries_processed'), byteorder='big')
-        except KeyError:
-            self.entries_processed = 0
+        self.entries_processed = int.from_bytes(self.item_store.get(b'entries_processed') or b'', byteorder='big')
 
 
     def fetch_address(self, uprn):
@@ -63,7 +60,7 @@ class RsfProcessor(object):
 
 
     def resolve_record(self, item_hash):
-        item_data = self.item_store.Get(item_hash)
+        item_data = self.item_store.get(item_hash)
         item = json.loads(item_data.decode('utf-8'))
         address = None
         if 'address' in item:
@@ -87,7 +84,8 @@ class RsfProcessor(object):
 
 
     def resolve_records(self):
-        for code, item_hash in self.item_store.RangeIter(key_from=b'prison:', key_to=b'prison:ZZZ'):
+        for code in self.item_store.keys(b'prison:*'):
+            item_hash = self.item_store.get(code)
             self.pool.spawn(self.resolve_record, item_hash)
         self.pool.join()
 
@@ -99,11 +97,11 @@ class RsfProcessor(object):
     def add_item(self, item_json):
         hash = b'sha-256:' + hexlify(hashlib.sha256(item_json).digest())
         print('putting ' + str(hash))
-        self.item_store.Put(hash, item_json)
+        self.item_store.set(hash, item_json)
 
 
     def append_entry(self, timestamp, item_hash, key):
-        self.item_store.Put(b'prison:'+key, item_hash)
+        self.item_store.set(b'prison:'+key, item_hash)
 
         self.entries_processed += 1            
 
@@ -122,7 +120,7 @@ class RsfProcessor(object):
     def close(self):
         self.pgconn.close()
         # item_store?
-        self.item_store.Put(b'entries_processed', self.entries_processed.to_bytes(8,byteorder='big'))
+        self.item_store.set(b'entries_processed', self.entries_processed.to_bytes(8,byteorder='big'))
 
 
 
