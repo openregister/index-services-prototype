a41f377c216a8d8c78c33c93b2231afa85701c55
Dan Mitchell <dan@digitalreflow.co.uk>
2017-03-07 11:19:40 +0000

    Add UPRN by default to address column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210082759) do
+ActiveRecord::Schema.define(version: 20170306220104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170210082759) do
     t.date     "opened"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "address_uprn"
   end
 
 end
